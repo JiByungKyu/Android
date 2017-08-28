ae66713b4e098af5491aa459fcc10389c4790e9a
KH <gh920708@naver.com>
2017-08-28 15:41:31 +0900

    8/28 UI

--- app/src/main/java/com/example/user/myapplication/AnalogActivity.java
+++ app/src/main/java/com/example/user/myapplication/AnalogActivity.java
@@ -13,10 +13,20 @@ public class AnalogActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_analog);
+
+        ListView analogListView = (ListView) findViewById(R.id.analogList);
+        AnalogActivityListAdapter analogListAdapter = new AnalogActivityListAdapter();
+        analogListView.setAdapter(analogListAdapter);
+        setListData(analogListAdapter);
     }
 
-    /*ListView  = (ListView) findViewById();
-    GraphListViewAdapter listAdapter = new GraphListViewAdapter();
-        listView.setAdapter(listAdapter);
-    setListData(listAdapter);*/
+    private void setListData(AnalogActivityListAdapter analogListAdapter){
+        analogListAdapter.addItem("Acceleration Pedal Position", "0.0", 0);
+        analogListAdapter.addItem("Alternator Voltage(V)", "9.5", 9.5/40);
+        analogListAdapter.addItem("Arm in Pressure(bar)", "0.0", 0);
+        analogListAdapter.addItem("Arm out Pressure(bar)", "0.0", 0);
+        analogListAdapter.addItem("Battery Voltage(V)", "28.4", 28.4/40);
+        analogListAdapter.addItem("Boom Down Pressure(bar)", "0.0", 0);
+        analogListAdapter.addItem("Boom Pressure(bar)", "279.0", 479/500);
+    }
 }
